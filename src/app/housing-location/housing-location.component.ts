d86d92d44ba8924f67a5d54345195a51a2337a8d
javierbarbozam <barboza.javier497@gmail.com>
2024-01-22 00:01:11 -0600

    add input and comment for understanding

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -12,5 +14,11 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  /*
+  El signo de exclamación (!) en TypeScript asegura que la propiedad será asignada antes de ser utilizada.
+  Se usa aquí porque la inicialización de la propiedad de entrada sucede después de la creación del componente,
+  con valores proporcionados desde el componente contenedor.
+  https://angular.io/tutorial/first-app/first-app-lesson-05
+  */
+  @Input() housingLocation!: HousingLocation;
 }
