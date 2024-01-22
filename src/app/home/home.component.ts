dd83fcdc6aa5be1e53baea9cee1e850a8de14c50
javierbarbozam <barboza.javier497@gmail.com>
2024-01-21 22:54:18 -0600

    Add html layout to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
   `,
-  styleUrl: './home.component.css'
+  styleUrl: "./home.component.css",
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
