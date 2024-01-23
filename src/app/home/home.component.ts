02134b4ac8e4731d8b88913b52fe02443b7be3f2
javierbarbozam <barboza.javier497@gmail.com>
2024-01-22 18:28:51 -0600

    Add input filter method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,8 +11,14 @@ import { HousingService } from "../housing.service";
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          Search
+        </button>
       </form>
     </section>
     <section class="results">
@@ -21,10 +27,12 @@ import { HousingService } from "../housing.service";
         that the assigned value should be treated as a property from the component class and not a string value.
       -->
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation"
-      >
-      </app-housing-location>
+      ></app-housing-location>
+      <!--
+        filteredLocationList is used to render every value, or just the filtered
+      -->
     </section>
   `,
   styleUrl: "./home.component.css",
@@ -32,8 +40,22 @@ import { HousingService } from "../housing.service";
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) =>
+        housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
